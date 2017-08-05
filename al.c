d18e77bdde8c0de9334e8a88ca6119f98d7b630b
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2017-08-05 16:49:22 +0530

    Create al.c

---
+++ al.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter the char:");
+scanf(%c",&a);
+if((a>'a'&&a<'z'))
+{
+printf("the given char is alphabet");
+}
+else
+{
+printf("not a alphabet");
+}
+}
